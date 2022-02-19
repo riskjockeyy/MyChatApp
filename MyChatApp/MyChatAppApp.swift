d14d436fe76a0cab5e274c0ed8243ad8d6d47c88
Ankita-Rawat <go2ankitarawat@gmail.com>
2022-02-20 02:02:51 +0530

    Initial Commit

---
+++ MyChatApp/MyChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyChatAppApp.swift
+//  MyChatApp
+//
+//  Created by Bharat Singh on 20/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MyChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
